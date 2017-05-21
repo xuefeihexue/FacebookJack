2c828333677847a0015c46e4efd9cb7ca8d7a802
Navtej Singh <navtejsaggu90@gmail.com>
2017-05-21 08:32:25 -0400

    Profile screen initial commit.

--- app/src/main/java/com/example/a1691717/facebookjack/navtejPackage/profileScreen.java
+++ app/src/main/java/com/example/a1691717/facebookjack/navtejPackage/profileScreen.java
@@ -1,11 +1,14 @@
 package com.example.a1691717.facebookjack.navtejPackage;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.a1691717.facebookjack.R;
 
@@ -15,17 +18,15 @@ public class profileScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_screen);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        final TextView button =(TextView)findViewById(R.id.whattextview3);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+               Intent i=new Intent(profileScreen.this,UserScreen.class);
+                startActivity(i);
             }
         });
+
     }
+
 
 }
